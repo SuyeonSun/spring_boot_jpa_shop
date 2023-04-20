fafd17103df92e1a7aa149c146a151e5ceeb482c
SuyeonSun <suninisy@gmail.com>
2023-04-21 08:55:00 +0900

    회원 엔티티 1차 구현

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter @Setter
+@Entity
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // order 테이블의 member 필드에 의해 mapped 된 것
+    private List<Order> orders = new ArrayList<>();
+}
